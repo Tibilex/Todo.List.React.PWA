ad2aff847db66bab1476cdb53d28ce6711bf4ef2
Tibilex <45212175+Tibilex@users.noreply.github.com>
2023-07-21 21:55:17 +0300

    Refactoring PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,9 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    manifest: {
-      name: 'Vite+TypeScript+PWA',
-      description: 'test PWA',
-      theme_color: '#ffffff',
-      icons: [{
-        src: '/android192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: '/android512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-      }]
-    },
-  })
+  plugins: [react(),
   ],
   base: '/',
 })
