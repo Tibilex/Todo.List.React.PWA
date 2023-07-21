8b773c68b70a56a80899cb9ebc301f2fcccd8257
Tibilex <45212175+Tibilex@users.noreply.github.com>
2023-07-21 23:11:12 +0300

    manifest

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,38 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })
+  plugins: [react(), VitePWA({
+    registerType: "autoUpdate", manifest: {
+      name: 'Todo PWA',
+      short_name: 'PWA',
+      description: 'First todo list application based on: Typescript and React',
+      theme_color: '#2c3440',
+      background_color: '#2c3440',
+      display: 'standalone',
+      icons: [
+        {
+          src: '/icon/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/icon/icon-256x256.png',
+          sizes: '256x256',
+          type: 'image/png'
+        },
+        {
+          src: '/icon/icon-384x384.png',
+          sizes: '384x384',
+          type: 'image/png'
+        },
+        {
+          src: '/icon/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })
   ],
   base: '/',
 })
